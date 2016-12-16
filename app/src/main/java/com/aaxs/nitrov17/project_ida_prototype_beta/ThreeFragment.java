ef84883f7ad6c852ef70d3c96cad7e29f7e92b0e
Amir Ayub <dev.amirayub@gmail.com>
2016-12-16 14:34:00 +0400

    Initial commit

---
+++ app/src/main/java/com/aaxs/nitrov17/project_ida_prototype_beta/ThreeFragment.java
@@ -0,0 +1,29 @@
+package com.aaxs.nitrov17.project_ida_prototype_beta;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import static com.aaxs.nitrov17.project_ida_prototype_beta.R.id.info;
+
+
+public class ThreeFragment extends Fragment{
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+
+}
